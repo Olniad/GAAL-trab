f04e9f5d5289f407828938e2dbcc49374aeed652
Danilo <danilo.martins333326@gmail.com>
2024-11-06 17:43:05 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import LineEquation from './components/LineEquation';
+import PlaneEquation from './components/PlaneEquation';
+import PointToPlaneDistance from './components/PointToPlaneDistance';
+import PointToLineDistance from './components/PointToLineDistance';
+import PlaneToPlaneDistance from './components/PlaneToPlaneDistance';
+import LineToLineDistance from './components/LineToLineDistance';
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Geometria Analítica e Algebra Linear.</h1>
+        <nav>
+          <button onClick={() => setSelectedOption("line")}>Equação da Reta</button>
+          <button onClick={() => setSelectedOption("plane")}>Equação do Plano</button>
+          <button onClick={() => setSelectedOption("pointToPlane")}>Distância Ponto-Plano</button>
+          <button onClick={() => setSelectedOption("pointToLine")}>Distância Ponto-Reta</button>
+          <button onClick={() => setSelectedOption("planeToPlane")}>Distância Plano-Plano</button>
+          <button onClick={() => setSelectedOption("lineToLine")}>Distância Reta-Reta</button>
+        </nav>
+        <div className="content">
+          {selectedOption === "line" && <LineEquation />}
+          {selectedOption === "plane" && <PlaneEquation />}
+          {selectedOption === "pointToPlane" && <PointToPlaneDistance />}
+          {selectedOption === "pointToLine" && <PointToLineDistance />}
+          {selectedOption === "planeToPlane" && <PlaneToPlaneDistance />}
+          {selectedOption === "lineToLine" && <LineToLineDistance />}
+        </div>
       </header>
     </div>
   );
